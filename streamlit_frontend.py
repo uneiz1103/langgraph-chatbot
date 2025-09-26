6223f9b9b0a89c7e9269859a65cb3b660c3c144e
uneiz1103 <uneizshaikh1103@gmail.com>
2025-09-26 13:24:18 +0530

    fix: correct response key to use 'messages' in Streamlit frontend

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -2,6 +2,7 @@ import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
+# st.session_state -> dict -> 
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
 if 'message_history' not in st.session_state:
@@ -12,19 +13,19 @@ for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
-user_input = st.chat_input('Type Here')
+user_input = st.chat_input('Type here')
 
 if user_input:
 
+    # first add the message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
-    response =chatbot.invoke({'messages': [HumanMessage(content = user_input)]}, config=CONFIG)
-    ai_message = response['message'][-1].content
-
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    
+    ai_message = response['messages'][-1].content
+    
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
     with st.chat_message('assistant'):
         st.text(ai_message)
-
-
