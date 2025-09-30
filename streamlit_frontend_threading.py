d9fbfdde6e96b0157ba3832dcb1f9b2aba1c6ff7
uneiz1103 <uneizshaikh1103@gmail.com>
2025-09-30 10:38:21 +0530

    feat(chat): add thread ID management and reset chat functionality in Streamlit UI

--- streamlit_frontend_threading.py
+++ streamlit_frontend_threading.py
@@ -1,17 +1,34 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
+
+# utility functions
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    st.session_state['message_history'] = []
 
 # Session Setup
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = []
 # Sidebar UI 
 st.sidebar.title('LangGraph Chatbot')
 
-st.sidebar.button('New Chat')
+if st.sidebar.button('New Chat'):
+    reset_chat()
 
 st.sidebar.header('Past Conversations')
+
+st.sidebar.text(st.session_state['thread_id'])
 
 # loading the conversation history
 for message in st.session_state['message_history']:
@@ -27,12 +44,13 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
     
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
     # st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
     with st.chat_message('assistant'):
          ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in chatbot.stream(
                 {'messages': [HumanMessage(content=user_input)]},
-                config= {'configurable': {'thread_id': 'thread-1'}},
+                config= CONFIG,
                 stream_mode= 'messages'
             )
         )
